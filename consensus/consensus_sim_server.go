e169eac05f9df8c6fb00bae046db41909c643135
Vedaad Shakib <vedaad799@gmail.com>
2018-07-24 15:41:18 -0700

    Initial sim 2

--- consensus/consensus_sim_server.go
+++ consensus/consensus_sim_server.go
@@ -4,7 +4,7 @@
 // permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
 // License 2.0 that can be found in the LICENSE file.
 
-package main
+package consensus
 
 import (
 	"context"
@@ -21,150 +21,26 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
-	"github.com/iotexproject/iotex-core-internal/actpool"
-	"github.com/iotexproject/iotex-core-internal/blockchain"
-	"github.com/iotexproject/iotex-core-internal/blocksync"
-	"github.com/iotexproject/iotex-core-internal/consensus"
-	"github.com/iotexproject/iotex-core-internal/delegate"
-	"github.com/iotexproject/iotex-core-internal/logger"
-	"github.com/iotexproject/iotex-core-internal/network"
-	"github.com/iotexproject/iotex-core-internal/pkg/hash"
-	pb "github.com/iotexproject/iotex-core-internal/simulator/proto/simulator"
-	"github.com/iotexproject/iotex-core-internal/config"
-	"github.com/iotexproject/iotex-core-internal/iotxaddress"
+	pb "github.com/tendermint/tendermint/simulator/proto/simulator"
 )
 
 const (
 	port         = ":50051"
 	dummyMsgType = 1999
-	rolldposConfig = "./config_local_rolldpos_sim.yaml"
 )
 
 // server is used to implement message.SimulatorServer.
 type (
 	server struct {
-		nodes []consensus.Sim // slice of Consensus objects
-	}
-	byzVal struct {
-		val blockchain.Validator
+		nodes []*ConsensusState // slice of Consensus objects
 	}
 )
 
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
-// Validate for the byzantine node uses the actual block validator and returns the opposite
-func (v *byzVal) Validate(blk *blockchain.Block, tipHeight uint64, tipHash hash.Hash32B) error {
-	//err := v.val.Validate(blk, tipHeight, tipHash)
-	//if err != nil {
-	//	return nil
-	//}
-	//return errors.New("")
-	return nil
-}
-
 // Ping implements simulator.SimulatorServer
 func (s *server) Init(in *pb.InitRequest, stream pb.Simulator_InitServer) error {
 	nPlayers := in.NBF + in.NFS + in.NHonest
-	ctx := context.Background()
-
-	var addrs []*iotxaddress.Address
-	var rawAddrs []string
-
-	for i := 0; i < int(nPlayers); i++ {
-		// create public/private key pair and address
-		addr, err := iotxaddress.NewAddress(iotxaddress.IsTestnet, iotxaddress.ChainID)
-		if err != nil {
-			logger.Error().Err(err).Msg("failed to create public/private key pair together with the address derived.")
-		}
-		addrs = append(addrs, addr)
-		rawAddrs = append(rawAddrs, addr.RawAddress)
-	}
-
-	fmt.Println(addrs)
-	fmt.Println(rawAddrs)
-
-	for i := 0; i < int(nPlayers); i++ {
-		//cfg := config.Default
-		//// s.nodes = make([]consensus.Sim, in.NPlayers)
-		//// allocate all the necessary space now because otherwise nodes will get copied and create pointer issues
-		//cfg.Consensus.Scheme = config.RollDPoSScheme
-		//cfg.Consensus.RollDPoS.DelegateInterval = time.Millisecond
-		//cfg.Consensus.RollDPoS.ProposerInterval = 0
-		//cfg.Consensus.RollDPoS.UnmatchedEventTTL = 1000 * time.Second
-		//cfg.Consensus.RollDPoS.RoundStartTTL = 1000 * time.Second
-		//cfg.Consensus.RollDPoS.AcceptProposeTTL = 1000 * time.Second
-		//cfg.Consensus.RollDPoS.AcceptPrevoteTTL = 1000 * time.Second
-		//cfg.Consensus.RollDPoS.AcceptVoteTTL = 1000 * time.Second
-		//cfg.Consensus.RollDPoS.ProposerCB = "PseudoRotatedProposer"
-		//cfg.Consensus.RollDPoS.ProposerCB = "PseudoStarNewEpoch"
-
-		config.Path = rolldposConfig
-		cfg, err := config.New()
-		if err != nil {
-			logger.Error().Err(err).Msg("could not find config file")
-		}
-
-		// handle node address, delegate addresses, etc.
-		cfg.Delegate.Addrs = rawAddrs
-		cfg.Network.Addr = "127.0.0.1:10000"
-		cfg.Network.NumPeersLowerBound = 6
-		cfg.Network.NumPeersUpperBound = 12
-
-		cfg.Chain.ProducerPrivKey = hex.EncodeToString(addrs[i].PrivateKey)
-		cfg.Chain.ProducerPubKey = hex.EncodeToString(addrs[i].PublicKey)
-
-		// set chain database path
-		cfg.Chain.ChainDBPath = "./chain" + strconv.Itoa(i) + ".db"
-		cfg.Chain.TrieDBPath = "./trie" + strconv.Itoa(i) + ".db"
-
-		bc := blockchain.NewBlockchain(cfg, blockchain.DefaultStateFactoryOption(), blockchain.BoltDBDaoOption())
-
-		if i >= int(in.NFS+in.NHonest) { // is byzantine node
-			val := bc.Validator()
-			byzVal := &byzVal{val: val}
-			bc.SetValidator(byzVal)
-		}
-
-		overlay := network.NewOverlay(&cfg.Network)
-		ap, err := actpool.NewActPool(bc, cfg.ActPool)
-		if err != nil {
-			logger.Fatal().Err(err).Msg("Fail to create actpool")
-		}
-
-		dlg := delegate.NewConfigBasedPool(&cfg.Delegate)
-		bs, _ := blocksync.NewBlockSyncer(cfg, bc, ap, overlay)
-		err = bs.Start(ctx)
-		if err != nil {
-			logger.Fatal().Err(err).Msg("Fail to start blocksync")
-		}
-
-		var node consensus.Sim
-		if i < int(in.NHonest) {
-			node = consensus.NewSim(cfg, bc, bs, dlg)
-		} else if i < int(in.NHonest+in.NFS) {
-			s.nodes = append(s.nodes, nil)
-			continue
-		} else {
-			node = consensus.NewSimByzantine(cfg, bc, bs, dlg)
-		}
-
-		s.nodes = append(s.nodes, node)
-
-		err = node.Start(ctx)
-		if err != nil {
-			logger.Fatal().Err(err).Msg("Fail to start node")
-		}
-
-		fmt.Printf("Node %d initialized and consensus engine started\n", i)
-	}
-
-	for i := 0; i < int(in.NFS); i++ {
-		s.nodes = append(s.nodes, nil)
-	}
-
-	fmt.Printf("Simulator initialized with %d players\n", nPlayers)
-
-	time.Sleep(5*time.Second) // TODO: do a waitUntil instead of sleeping
 
 	return nil
 }
@@ -173,27 +49,6 @@ func (s *server) Init(in *pb.InitRequest, stream pb.Simulator_InitServer) error 
 func (s *server) Ping(in *pb.Request, stream pb.Simulator_PingServer) error {
 	fmt.Println()
 
-	fmt.Printf("Node %d pinged; opened message stream\n", in.PlayerID)
-	msgValue, err := hex.DecodeString(in.Value)
-	if err != nil {
-		logger.Error().Msg("Could not decode message value into byte array")
-	}
-
-	done := make(chan bool)
-
-	s.nodes[in.PlayerID].SetStream(&stream)
-	s.nodes[in.PlayerID].SendUnsent()
-
-	// message type of 1999 means that it's a dummy message to allow the engine to pass back proposed blocks
-	if in.InternalMsgType != dummyMsgType {
-		msg := consensus.CombineMsg(in.InternalMsgType, msgValue)
-		err = s.nodes[in.PlayerID].HandleViewChange(msg, done)
-		if err != nil {
-			logger.Error().Err(err).Msg("failed to handle view change")
-		}
-		time.Sleep(2 * time.Millisecond)
-		<-done // wait until done
-	}
 
 	fmt.Println("closed message stream")
 	return nil
